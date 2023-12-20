0bc3aba440e51517096ab023fccee65e4fd6db8e
Aravinda87 <aravinda.turuvekere@gmail.com>
2023-12-20 12:44:55 +0530

    new

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+        // let text = ["aravinda "," rajashree "," sahana"];
+        // document.getElementById("demo").innerHTML = text ;
+
+        // function myfunction(){
+        //     return "hello" ;
+        // }
+
+        let cars = new Array("aravinda "," rajashree ", 46);
+        let text = " ";
+        for(let i = 0; i < cars.length; i++ )
+        {
+            text += cars[i] ;
+        }
+        document.getElementById("demo").innerHTML = text ;
