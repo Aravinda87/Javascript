2c74a8229ad834f94e700267e0714cc75b448269
Aravinda87 <aravinda.turuvekere@gmail.com>
2024-02-15 02:13:54 +0530

    STREAMS AND PIPES

--- NODE-JS/index.js
+++ NODE-JS/index.js
@@ -1,28 +1,15 @@
-const file_system  = require("node:fs")
+const fs = require("node:fs")
 
-const fs = file_system.readFileSync("./Drink.js","utf-8")
+const readablestream = fs.createReadStream("./file1.txt",{
+    encoding : "utf-8",
+    highWaterMark : 2,
+});
 
-const async = file_system.readFile("./Drink.js","utf-8",(error,data)=>{
-    if(error)
-    {
-        console.log(error)
-    }else{
-        console.log(data)
-    }
-})
+const writeablestram = fs.createWriteStream("./file2.txt");
 
-console.log(fs)
-console.log(async)
-console.log("-----------------------------------------------------------")
+readablestream.pipe(writeablestram);
 
-file_system.writeFileSync("./aru.txt","hello aru")
-
-file_system.writeFile("./aru.txt","aru", (err) =>{
-    if(err)
-    {
-        console.log(err)
-    }else{
-        console.log("file written")
-    }
-})
-console.log("-----------------------------------------------------------")
+// readablestream.on("data", (chunk) =>{
+//     console.log(chunk)
+//     writeablestram.write(chunk)
+// });
