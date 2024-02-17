2b3283e260932a0da8f0a46841ec0a5a4863cfc4
Aravinda87 <aravinda.turuvekere@gmail.com>
2024-02-17 19:40:40 +0530

    node js complete

--- NODE-JS/index.js
+++ NODE-JS/index.js
@@ -1,37 +1,9 @@
-const http = require("node:http")
 const fs = require("node:fs")
 
-const server = http.createServer((req,res) =>{
+setTimeout(() => {
+    console.log("timer")
+}, 5000);
 
-    // res.end(req.url)
-
-    if(req.url === "/")
-    {
-        res.writeHead(200 , {"content-type" : "text/plain"})
-        res.end("home-page")
-    }else if(req.url === "/about")
-    {
-        res.writeHead(200 , {"content-type" : "text/plain"})
-        res.end("about-page")
-    }else if(req.url === "/api")
-    {
-        res.writeHead(200 , {"content-type" : "application/json"})
-        res.end(JSON.stringify({
-            first : "aravinda",
-            second : "T R"
-        }))
-    }else{
-        res.writeHead(404)
-        res.end("page not found!")
-    }
-
-    // res.writeHead(200,{"content-type":"text/html"})
-    // const html = fs.readFileSync("./index.html","utf-8")
-    // res.end(html)
-
+fs.readFile(__filename,() =>{
+    console.log("i/o queue")
 })
-
-server.listen(3000,() =>{
-        console.log("server running on http://localhost:3000/")
-})
-
